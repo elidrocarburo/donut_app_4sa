003f47a5b921d9ca87806db5882afebacdc0c514
Adriana Cervera Rivadeneyra <princeritsu2209@outlook.com>
2025-02-10 12:51:47 -0600

    AddBar added

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget { //los datos que va cambiando el usuario y por lo mismo se van actualizando (cuando necesitamos cambios)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        //icono de la izquierda
+        leading: Icon(
+          Icons.menu, 
+          color: Colors.grey[800]
+          ),
+        actions: [
+          Icon(Icons.person)
+          ],
+      ),
+    );
+  }
+}
