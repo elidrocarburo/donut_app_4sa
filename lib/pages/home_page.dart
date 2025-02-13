b23b9da17bed8b50c050f588db9aa2fae4dc8b0a
Adriana Cervera Rivadeneyra <princeritsu2209@outlook.com>
2025-02-13 13:32:43 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,9 +19,40 @@ class _HomePageState extends State<HomePage> {
           color: Colors.grey[800]
           ),
         actions: [
-          Icon(Icons.person)
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          )
           ],
       ),
+      body: Column(
+        children: [
+        //1. Texto principal (Text)
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            children: [
+              Text('I want to ', style: TextStyle(
+                fontSize: 32
+              ),),
+              Text('Eat', style: TextStyle(
+                //Tamaño de letra
+                fontSize: 32,
+                // Negritas
+                fontWeight: FontWeight.bold,
+                //Subrayado
+                decoration: TextDecoration.underline,
+              ),),
+            ],
+          ),
+        )
+        //2. Pestañas (TabBar)
+
+        //3. Contenido de pestañas (TapBarView)
+
+        //4. Carrito (Cart)
+        ],
+      )
     );
   }
 }
